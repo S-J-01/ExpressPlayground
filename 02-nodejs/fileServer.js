eaead2833397a6fc4f7c20d3981a3e2a7e8bc553
Shreyas Jaltare <shreyasjaltare@gmail.com>
2024-03-12 14:51:13 +0530

    adding code for fileServer

--- 02-nodejs/fileServer.js
+++ 02-nodejs/fileServer.js
@@ -21,5 +21,31 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
+app.get('/files',(req,res)=>{
+  fs.readdir('./files',(err,files)=>{
+    if(err){
+      res.status(500).send(`error in reading`);
+    }else{
+      res.status(200).json(files);
+    }
+  })
+})
+
+app.get('/file/:filename',(req,res)=>{
+  var filename = req.params.filename;
+
+  var filePath = './files/'+filename;
+  fs.readFile(filePath,'utf-8',(err,data)=>{
+    if(err){
+      res.status(404).send(`File not found`);
+    }else{
+      res.status(200).send(data);
+    }
+  })
+})
+
+app.use((req,res,next)=>{
+  res.status(404).send(`Route not found`);
+})
 
 module.exports = app;
